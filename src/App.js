efb57ed885c1f228dc406c8e1b6f6c86b21ae386
davidruncode <daguz.91@gmail.com>
2022-09-11 16:16:26 -0400

    Pantalla inicial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container text-center">
+      <div class="row justify-content-md-center">
+        <div className='col-6 m-3'>
+          <img src='logoBV.png' alt='Biblioteca Virtual UNIES'></img>
+          <form>
+            <div className='input-group mb-3'>
+              <input 
+                type="text" 
+                className='form-control' 
+                placeholder='¿Qué recurso deseas buscar?'
+                aria-describedby="button-search">
+              </input>
+              <button
+                className='btn btn-success'
+                id="button-search">
+                Buscar
+              </button>
+            </div>
+          </form>
+          <a href='#'>Busqueda Avanzada</a>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
